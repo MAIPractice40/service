d5fb11982331f38c8a4b6e298766cf049356da38
Peter1811 <78861235+Peter1811@users.noreply.github.com>
2023-04-20 16:15:02 +0300

    Added models to admin pannel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Application, Contract, Customer
+
+admin.site.register(Application)
+admin.site.register(Contract)
+admin.site.register(Customer)
 
 # Register your models here.
