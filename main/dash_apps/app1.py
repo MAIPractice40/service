c74b58bc7f9aaab5226d41e33640336a113f2203
Vladislav <90531033+molch4nov@users.noreply.github.com>
2023-06-13 23:29:56 +0300

    almost without dash
    Сильвестр, мне пизда

--- main/dash_apps/app1.py
+++ main/dash_apps/app1.py
@@ -64,6 +64,7 @@ external_stylesheets = [
         "href": "https://fonts.googleapis.com/css2?"
         "family=Lato:wght@400;700&display=swap",
         "rel": "stylesheet",
+
     },
 ]
 app = DjangoDash('Stats', external_stylesheets=external_stylesheets)
--- main/urls.py
+++ main/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('contract/create', CreateContractView.as_view(), name='contract_create'),
     path('contract/list', ContractListView.as_view(), name='contract_list'),
     path('contract/list/<int:pk>', ContractDetailView.as_view(), name='contract_detail'),
-    path('application/create', CreateApplicationView.as_view(), name='application_create'),
-    path('application/list', ApplicationListView.as_view(), name='application_list'),
-    path('application/list/<int:pk>', ApplicationDetailView.as_view(), name='application_detail'),
+    # path('application/create', CreateApplicationView.as_view(), name='application_create'),
+    # path('application/list', ApplicationListView.as_view(), name='application_list'),
+    # path('application/list/<int:pk>', ApplicationDetailView.as_view(), name='application_detail'),
     path('app1/', dash_demo)
 ]
