6a4baa994d36471b404221c8e84b0d61fcd768eb
Vladislav <90531033+molch4nov@users.noreply.github.com>
2023-06-21 16:41:25 +0300

    Date

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 
 from .models import Application, Contract
 
@@ -6,9 +7,14 @@ from .models import Application, Contract
 class ContractForm(ModelForm):
     class Meta:
         model = Contract
-        fields=['title', 'text', 'date_of_contract', 'data_file']
+        fields = ['title', 'text', 'date_of_contract', 'data_file']
+        widgets = {
+            'date_of_contract': forms.TextInput(attrs={'placeholder': 'YYYY-MM-DD'}),
+        }
+
 
 class ApplicationForm(ModelForm):
     class Meta:
         model = Application
-        fields = ['number', 'contract', 'date_of_contract', 'inn', 'test_object', 'defined_characteristic', 'amount', 'type_of_documentation']
+        fields = ['number', 'contract', 'date_of_contract', 'inn', 'test_object', 'defined_characteristic', 'amount',
+                  'type_of_documentation']
